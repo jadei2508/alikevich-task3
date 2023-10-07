fe3975f7f91503d97bf48248edc6a102f8efc20c
Roman Alikevich <roma.alikevich8@gmail.com>
2023-10-07 10:07:36 +0300

    Initial Commit

---
+++ task3/task3App.swift
@@ -0,0 +1,17 @@
+//
+//  task3App.swift
+//  task3
+//
+//  Created by Роман on 07/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct task3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
